edeba2f18d70b130c6b898bc1fcb4efc813fd1ed
Thiago Santos <thia.mdossantos@gmail.com>
2018-11-06 19:55:56 -0200

    docs: add graphql docs

--- lib/invault_web/graphql/accounts/mutations.ex
+++ lib/invault_web/graphql/accounts/mutations.ex
@@ -9,6 +9,7 @@ defmodule InvaultWeb.GraphQL.Accounts.Mutations do
   object :accounts_mutations do
     mutation do
       field :create_totp_secret, :totp_secret do
+        description "Create a totp secret to setup the 2FA for an account"
         resolve &Resolver.create_totp_secret/2
       end
     end
--- lib/invault_web/graphql/accounts/types.ex
+++ lib/invault_web/graphql/accounts/types.ex
@@ -5,13 +5,15 @@ defmodule InvaultWeb.GraphQL.Accounts.Types do
   use Absinthe.Schema.Notation
 
   object :totp_secret do
-    field :id, :id
-    field :secret, non_null(:string)
-    field :recovery_codes, list_of(:recovery_code)
+    description "A secret used to generate TOTP codes for 2FA authentication"
+    field :id, :id, description: "Unique identifier for a recovery code"
+    field :secret, non_null(:string), description: "TOTP Secret"
+    field :recovery_codes, list_of(:recovery_code), description: "List of totp codes for this totp secret"
   end
 
   object :recovery_code do
-    field :id, :id
-    field :used_at, :datetime
+    description "A code to recover a 2FA authentication"
+    field :id, :id, description: "Unique identifier for a recovery code"
+    field :used_at, :datetime, description: "When the recovery code was used"
   end
 end
